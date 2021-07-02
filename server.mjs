fe897df026d1674f10a1448dbb5318b37c77a451
LucPrestin <l.prestin@gmx.de>
2021-07-02 10:49:57 +0200

    fix wrong path handling

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
-const http = require("http")
-const fs = require("fs")
-const pitch_finder = require("./pitch-finding.js");
+import http from 'http'
+import fs from 'fs'
+import {test_pitch_finding} from "./pitch-finding.js"
 
 let fs_readFile = function(file) {
     return new Promise((resolve, reject) => fs.readFile(file, "utf8", (err, data) => {
@@ -14,30 +14,38 @@ let fs_readFile = function(file) {
 
 class Server {
     static async onRequest(req, res) {
-        if (req.url.match(/\//)) {
+        if (req.url.match(/\/pitch_finding_test/)) {
+            res.writeHead(200, {"Content-Type": "application/json"})
+            res.end(JSON.stringify(this._pitches))
+        } else if (req.url.match(/\//)) {
             res.writeHead(200, {'Content-Type': 'text/html'})
             res.end(await fs_readFile('./index.html'))
         }
+    }
 
-        if (req.url.match(/\/pitch_finding_test/)) {
-            res.writeHead(200, {'Content-Type': 'text/plain'})
-            let frequencies = pitch_finder.test_pitch_finding()
-            let result = JSON.stringify(frequencies)
-            res.end(result)
-        }
+    static async load() {
+        console.log('Loading started')
 
-        //res.end("NO MATCHES")
+        this._pitches = test_pitch_finding()
+
+        console.log('Loading finished')
     }
 
     static async start() {
+        console.log('Server boot started')
+
+        await this.load()
+
         this.port = 8080
         http.createServer((req, res) => this.onRequest(req, res))
             .listen(this.port, function (error) {
                 if (error) {
                     throw error
                 }
-            });
+            })
+
+        console.log('Server boot finished')
     }
 }
 
-Server.start()
+Server.start();
