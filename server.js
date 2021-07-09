31c977cdd48d42c8cb06f2b5fb904183b447c83f
LucPrestin <l.prestin@gmx.de>
2021-07-09 16:27:23 +0200

    update supported requests

--- server.js
+++ server.js
@@ -1,44 +1,35 @@
 const http = require('http')
 const fs_readFile = require('./data_functions/utils.js').fs_readFile
-const test_pitch_finding = require('./data_functions/pitch-finding.js').test_pitch_finding
-const test_midi_loading = require('./data_functions/midi_loading.js').test_midi_loading
+
+const {test_pitch_finding} = require("./data_functions/pitch_finding")
+const {test_midi_loading} = require("./data_functions/midi_loading");
 
 class Server {
     static async onRequest(req, res) {
-        if (req.url.match(/\/pitch_finding_test/)) {
+        console.log(`request: ${req.method} ${req.url}`)
+        if (req.url === '/') {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end(await fs_readFile('./view/index.html'))
+        } else if (req.url.match(/\/pitch_finding_test/)) {
             res.writeHead(200, {"Content-Type": "text/html"})
-            res.end(await fs_readFile('./pitch_finding_test.html'))
+            res.end(await fs_readFile('./view/pitch_finding_test.html'))
         } else if (req.url.match(/\/speech_synthesis_test/)) {
             res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(await fs_readFile('./speech_synthesis_test.html'))
+            res.end(await fs_readFile('./view/speech_synthesis_test.html'))
         } else if (req.url.match(/\/midi_input_test/)) {
             res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(await fs_readFile('./midi_input_test.html'))
-        } else if (req.url.match(/\/load_example_pitches/)) {
-            res.writeHead(200, {"Content-Type": "application/json"})
-            res.end(await test_pitch_finding())
-        } else if (req.url.match(/\/load_example_midi/)) {
-            res.writeHead(200, {"Content-Type": "application/json"})
-            res.end(await test_midi_loading())
-        } else if (req.url.match(/\//)) {
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(await fs_readFile('./index.html'))
+            res.end(await fs_readFile('./view/midi_input_test.html'))
+        } else if (req.url.match(/\/data\/load_example_pitches/)) {
+            res.writeHead(200, {'Content-Type': 'application/json'})
+            res.end(JSON.stringify(test_pitch_finding()))
+        } else if (req.url.match(/\/data\/load_example_midi/)) {
+            res.writeHead(200, {'Content-Type': 'application/json'})
+            res.end(JSON.stringify(test_midi_loading()))
         }
-    }
-
-    static async load() {
-        console.log('Loading started')
-
-        //this._pitches = test_pitch_finding()
-        this._pitches = {}
-
-        console.log('Loading finished')
     }
 
     static async start() {
         console.log('Server boot started')
-
-        await this.load()
 
         this.port = 8080
         http.createServer((req, res) => this.onRequest(req, res))
