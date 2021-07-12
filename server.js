ba058b22c76f5bbeedcde0d32be4b634777983e9
LucPrestin <l.prestin@gmx.de>
2021-07-12 10:53:28 +0200

    switch from plain javascript to express in order to use the body-parser

--- server.js
+++ server.js
@@ -1,46 +1,112 @@
-const http = require('http')
-const fs_readFile = require('./data_functions/utils.js').fs_readFile
+const express = require('express')
+const bodyParser = require('body-parser')
+const utils = require('./scripts/utils.js')
+const songify = require('./scripts/songify').songify
 
-const {test_pitch_finding} = require("./data_functions/pitch_finding")
-const {test_midi_loading} = require("./data_functions/midi_loading");
+const {test_pitch_finding} = require("./scripts/pitch_finding")
+const {test_midi_loading} = require("./scripts/midi_loading")
 
-class Server {
-    static async onRequest(req, res) {
-        console.log(`request: ${req.method} ${req.url}`)
-        if (req.url === '/') {
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(await fs_readFile('./view/index.html'))
-        } else if (req.url.match(/\/pitch_finding_test/)) {
-            res.writeHead(200, {"Content-Type": "text/html"})
-            res.end(await fs_readFile('./view/pitch_finding_test.html'))
-        } else if (req.url.match(/\/speech_synthesis_test/)) {
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(await fs_readFile('./view/speech_synthesis_test.html'))
-        } else if (req.url.match(/\/midi_input_test/)) {
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(await fs_readFile('./view/midi_input_test.html'))
-        } else if (req.url.match(/\/data\/load_example_pitches/)) {
-            res.writeHead(200, {'Content-Type': 'application/json'})
-            res.end(JSON.stringify(test_pitch_finding()))
-        } else if (req.url.match(/\/data\/load_example_midi/)) {
-            res.writeHead(200, {'Content-Type': 'application/json'})
-            res.end(JSON.stringify(test_midi_loading()))
-        }
-    }
+const app = express()
 
-    static async start() {
-        console.log('Server boot started')
-
-        this.port = 8080
-        http.createServer((req, res) => this.onRequest(req, res))
-            .listen(this.port, function (error) {
-                if (error) {
-                    throw error
-                }
-            })
-
-        console.log('Server boot finished')
-    }
+const processing_status = {
+    waiting_for_input: 0,
+    ready: 1,
+    done: 2
 }
 
-Server.start();
+const usage = 'Usage of this service:\n'+
+    '1. request an id\n' +
+    '2. send a text and midi file with your id via a post request to /songifying_parameters\n' +
+    '3. send a get request to /songified_text with your id'
+
+/*
+* format:
+* id: {
+*   status: <a processing_status value>,
+*   text: <the text that is to songify>,
+*   midi: <the midi data from which to take the melody>
+*   result: <the resulting wav data>
+* }
+* */
+let processing_results = {}
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', async (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end(await utils.fs_readFile('./view/index.html'))
+})
+
+app.get('/processing_id', async (req, res) => {
+    res.writeHead(200, {"Content-Type": "application/json"})
+    const id = utils.get_new_id(processing_results)
+    processing_results[id] = {
+        status: processing_status.waiting_for_input,
+        midi: null,
+        text: null
+    }
+    res.end(id)
+})
+
+app.get('/pitch_finding_test', async (req, res) => {
+    res.writeHead(200, {"Content-Type": "text/html"})
+    res.end(await utils.fs_readFile('./view/pitch_finding_test.html'))
+})
+
+app.get('/speech_synthesis_test', async (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end(await utils.fs_readFile('./view/speech_synthesis_test.html'))
+})
+
+app.get('/midi_input_test', async (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end(await utils.fs_readFile('./view/midi_input_test.html'))
+})
+
+app.get('/data/load_example_pitches', async (req, res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(test_pitch_finding()))
+})
+
+app.get('/data/load_example_midi', async (req, res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify(test_midi_loading()))
+})
+
+app.get('/songified_text', async (req, res) => {
+    const id = req.body.id
+    if (!Object.keys(processing_status).includes(id)) {
+        res.writeHead(400, {'Content-Type': 'text/plain'})
+        res.end('Id not found.' + usage)
+    } else {
+        const processing_result = processing_results[id]
+        switch (processing_result.status) {
+            case processing_status.waiting_for_input:
+                res.writeHead(400, {'Content-Type': 'text/plain'})
+                res.end('Missing input data.' + usage)
+                break
+            case processing_status.ready:
+                processing_result.result = await songify(processing_result.text, processing_result.midi)
+                processing_result.status = processing_status.done
+                res.writeHead(200, {'Conent-Type': 'audio/wav'})
+                res.end(processing_result.result)
+                break
+            case processing_status.done:
+                res.writeHead(200, {'Conent-Type': 'audio/wav'})
+                res.end(processing_result.result)
+                break
+        }
+    }
+})
+
+app.post('/songifying_parameters', async (req, res) => {
+    const processing_result = processing_results[req.body.id]
+    processing_result.text = req.body.text
+    processing_result.midi = req.body.midi
+    processing_result.status = processing_status.ready
+    res.writeHead(200)
+    res.end()
+})
+
+app.listen(8080)
