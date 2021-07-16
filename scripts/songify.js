b864a174ea17bfcc88312c2627f9cc7f595e4e00
LucPrestin <l.prestin@gmx.de>
2021-07-16 15:04:39 +0200

    map phonemes to notes

--- scripts/songify.js
+++ scripts/songify.js
@@ -1,13 +1,15 @@
 const spawn = require('child_process').spawn
 
 async function songify (text, midi) {
-    //get phonemes from the text
-    let phonemes = await split_into_phonemes(text)
+    // Get phonemes from the text
+    const phonemes = await split_into_phonemes(text)
 
-    //Doing: Get pitches and durations from the midi file
-    let notes = midi.tracks[0].notes
+    // Get pitches and durations from the midi file
+    const notes = midi.tracks[0].notes
 
-    //TODO: map notes to phonemes
+    // map notes to phonemes
+    const phoneme_note_map = map_phonemes_to_notes(phonemes, notes)
+
     //TODO: generate sung version of each phoneme in its note
     //TODO: concatenate sung phonemes to a song
     //TODO: return song
@@ -18,6 +20,42 @@ async function songify (text, midi) {
     } else {
         return []
     }
+}
+
+function map_phonemes_to_notes(phonemes, notes) {
+    const result = []
+
+    let note_index = 0
+
+    for (let phoneme_index = 0; phoneme_index < phonemes.length; phoneme_index++) {
+        const phoneme = phonemes[phoneme_index]
+        if (is_vowel(phoneme)) {
+            result.push({
+                phoneme: phoneme,
+                note: notes[note_index]
+            })
+            note_index++
+        } else {
+            result.push({
+                phoneme: phoneme,
+                note: null
+            })
+        }
+    }
+
+    return result
+}
+
+function is_vowel(phoneme) {
+    const vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+
+    for (let i = 0; i < vowels.length; i++) {
+        if (phoneme.includes(vowels[i])) {
+            return true
+        }
+    }
+
+    return false
 }
 
 function split_into_phonemes(text) {
@@ -31,7 +69,14 @@ function split_into_phonemes(text) {
 
         ls.on('exit', code => {
             if (code === 0) {
-                resolve(out)
+                let result = out[0]
+                result = result.replace(/[\n\r]/g, '')
+
+                while(result.match(/'/)) {
+                    result = result.replace(/'/, '\"')
+                }
+
+                resolve(JSON.parse(result))
             } else {
                 reject(JSON.stringify(err))
             }
